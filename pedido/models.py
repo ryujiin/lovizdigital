74b0245360c24086b36bf32213cc3f8a6df04544
ryujiin <ryujiin22@gmail.com>
2015-12-09 08:55:20 -0500

    cambie el formato de fecha de paypal form para que el ipn simulater funcara

--- pedido/models.py
+++ pedido/models.py
@@ -98,3 +98,21 @@ class Pago(models.Model):
 
 	def __unicode__(self):
 		return self.id_pago
+
+from paypal.standard.models import ST_PP_COMPLETED
+from paypal.standard.ipn.signals import valid_ipn_received,payment_was_successful
+
+def show_me_the_money(sender, **kwargs):
+	ipn_obj = sender
+	if ipn_obj.payment_status == ST_PP_COMPLETED:
+		print 'Bien'
+		if ipn_obj.custom == "Comprando los mejores productos!":
+			pago = Pago(cantidad = ipn_obj.mc_gross,descripcion=ipn_obj.payment_status,transaccion = ipn_obj.txn_id)
+			metodo = MetodoPago.objects.get(nombre='Paypal')
+			pago.metodo_pago = metodo.id
+			pago.save()
+	else:
+		print 'Salio Mal'
+
+valid_ipn_received.connect(show_me_the_money)
+payment_was_successful.connect(show_me_the_money)

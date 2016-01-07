b0bd41fe3f09db5fcefd291a7720280a99d5824d
ryujiin <ryujiin22@gmail.com>
2016-01-07 00:10:56 -0500

    arreglado facebook

--- cliente/pipeline.py
+++ cliente/pipeline.py
@@ -4,7 +4,7 @@ from models import Cliente
 
 def save_profile_picture(backend, user, response, details,is_new=False,*args,**kwargs):
 
-	if backend.name == 'facebook':
+	if is_new and backend.name == 'facebook':
 		url = 'http://graph.facebook.com/{0}/picture'.format(response['id'])
 
 		try:

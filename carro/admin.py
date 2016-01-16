b7837199e28b0b4667d27aac080f9b6a987433aa
ryujiin <ryujiin22@gmail.com>
2016-01-15 21:25:44 -0500

    modificacion de admin carro

--- carro/admin.py
+++ carro/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from models import *
 
+class CarroAdmin (admin.ModelAdmin):
+	list_display = ('id','__unicode__','date_created','num_lineas')
+
 # Register your models here.
-admin.site.register(Carro)
+admin.site.register(Carro,CarroAdmin)
 admin.site.register(LineaCarro)
 
